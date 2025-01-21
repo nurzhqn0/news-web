caa6fad492cc3b56818fd85145d5aa668e208300
nurzhqn0 <nurzhqn@gmail.com>
2025-01-21 17:55:40 +0500

    boilerplate

---
+++ src/kz/bitlab/java/models/News.java
@@ -0,0 +1,11 @@
+package kz.bitlab.java.models;
+
+import java.util.Date;
+
+public class News {
+    private Long id;
+    private String title;
+    private String content;
+    private int languageId;
+    private Date postDate;
+}
